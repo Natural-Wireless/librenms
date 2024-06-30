0904b550ad9b0467323e37f411b26eb4c92eee9e
Ethan Hayon <ethan@naturalwireless.com>
2024-06-30 17:40:09 -0400

    nec ipasolink advanced ex discovery

---
+++ LibreNMS/OS/IpasolinkExAdvanced.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace LibreNMS\OS;
+
+use LibreNMS\Device\WirelessSensor;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessPowerDiscovery;
+use LibreNMS\Interfaces\Discovery\Sensors\WirelessRssiDiscovery;
+use LibreNMS\OS;
+
+class IpasolinkExAdvanced extends OS implements WirelessPowerDiscovery, WirelessRssiDiscovery
+{
+    public function discoverWirelessPower()
+    {
+        $oid = '.1.3.6.1.4.1.119.2.3.69.501.8.1.1.4.16842752';
+
+        return [
+            new WirelessSensor('power', $this->getDeviceId(), $oid, 'nec-ipasolink-ex-advanced', 0, 'Tx Power', null, 1, 10),
+        ];
+    }
+
+    public function discoverWirelessRssi()
+    {
+        $oid = '.1.3.6.1.4.1.119.2.3.69.501.8.1.1.6.16842752';
+        return [
+            new WirelessSensor('rssi', $this->getDeviceId(), $oid, 'nec-ipasolink-ex-advanced', 0, 'RSL', null, 1, 10),
+        ];
+    }
+}
